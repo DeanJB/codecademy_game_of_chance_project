252a07b142e074f27e39f8d572935c0d534ffab1
Dean Bevis <65736551+DeanJB@users.noreply.github.com>
2020-07-14 19:16:32 -0700

    Added function for coin toss game

--- games.py
+++ games.py
@@ -4,9 +4,21 @@ money = 100
 
 #Write your game of chance functions here
 
+def coin_toss(call, bet, money):
+    current_bet = bet
+    toss = random.randint(1, 2)
+    if toss == 1 and call == "Heads":
+        print("YOU WIN! The coin came up Heads. You have won ${bet}".format(bet = bet))
+        money += current_bet
+    elif toss == 2 and call == "Tails":
+        print("YOU WIN! The coin came up Tails. You have won ${bet}".format(bet = bet))
+        money += bet
+    else:
+        print("Sorry you lose. Please play again")
+        money -= bet
+    print(money)
 
-
-
+coin_toss("Heads", 30, money)
 
 
 #Call your game of chance functions here
