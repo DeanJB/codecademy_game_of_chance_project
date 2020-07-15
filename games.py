7e45691edb1aa5607ec89f1e746ca9bc62df636c
Dean Bevis <65736551+DeanJB@users.noreply.github.com>
2020-07-14 19:25:57 -0700

    Added function for Cho Han game.

--- games.py
+++ games.py
@@ -4,7 +4,7 @@ money = 100
 
 #Write your game of chance functions here
 
-def coin_toss(call, bet, money):
+def coin_toss(call, bet, money=money):
     current_bet = bet
     toss = random.randint(1, 2)
     if toss == 1 and call == "Heads":
@@ -16,9 +16,29 @@ def coin_toss(call, bet, money):
     else:
         print("Sorry you lose. Please play again")
         money -= bet
-    print(money)
+    print("Your new total is: ${money}.".format(money = money))
 
-coin_toss("Heads", 30, money)
+#Test for Coin Toss
+#coin_toss("Heads", 30)
+
+def cho_han(call, bet, money=money):
+    die_one = random.randint(1,6)
+    die_two = random.randint(1,6)
+    if (die_one + die_two)%2 == 0 and call == "Even":
+        print("YOU WIN! The dice fell as {die_one} and {die_two}. The total is {total}. You win {bet}.".format(die_one = die_one, die_two = die_two, total = (die_one + die_two), bet = bet))
+        money += bet
+    elif (die_one + die_two)%2 != 0 and call == "Odd":
+        print("YOU WIN! The dice fell as {die_one} and {die_two}. The total is {total}. You win {bet}.".format(die_one = die_one, die_two = die_two, total = (die_one + die_two), bet = bet))
+        money += bet
+    else:
+        print("Sorry, you lose. Better luck next time.")
+        money -= bet
+    print("Your new total is: ${money}".format(money = money))
+
+#Testing Cho Han game
+#cho_han("Odd", 40)
+
+
 
 
 #Call your game of chance functions here
